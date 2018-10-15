556844b6531e6e9fd5e1e80a54eccecffdc92552
andredcoliveira <up201405639@fe.up.pt>
2018-10-15 22:23:02 +0100

    Pretty

--- T1/t1/MyTriangle.js
+++ T1/t1/MyTriangle.js
@@ -3,7 +3,20 @@
  * @constructor
  */
 class MyTriangle extends CGFobject {
-	constructor(scene, x1 = 1, x2 = 0, x3 = -1, y1 = 0, y2 = 1, y3 = 0, z1 = 0, z2 = 0, z3 = 0, length_s = 1, length_t = 1) {
+	constructor(
+		scene,
+		x1 = 1,
+		x2 = 0,
+		x3 = -1,
+		y1 = 0,
+		y2 = 1,
+		y3 = 0,
+		z1 = 0,
+		z2 = 0,
+		z3 = 0,
+		length_s = 1,
+		length_t = 1
+	) {
 		super(scene);
 		this.x1 = x1;
 		this.x2 = x2;
@@ -18,7 +31,6 @@ class MyTriangle extends CGFobject {
 	}
 
 	initBuffers() {
-
 		/**
 		 * 					x1, y1, z1
 		 * 						/\
@@ -31,42 +43,51 @@ class MyTriangle extends CGFobject {
 		 * 	  x2, y2, z2 ------- c ------ x3, y3, z3
 		 */
 
-		let a = Math.sqrt((this.x1 - this.x3) ** 2, (this.y1 - this.y3) ** 2, (this.z1 - this.z3) ** 2);
-		let b = Math.sqrt((this.x2 - this.x1) ** 2, (this.y2 - this.y1) ** 2, (this.z2 - this.z1) ** 2);
-		let c = Math.sqrt((this.x3 - this.x2) ** 2, (this.y3 - this.y2) ** 2, (this.z3 - this.z2) ** 2);
+		let a = Math.sqrt(
+			(this.x1 - this.x3) * (this.x1 - this.x3),
+			(this.y1 - this.y3) * (this.y1 - this.y3),
+			(this.z1 - this.z3) * (this.z1 - this.z3)
+		);
+		let b = Math.sqrt(
+			(this.x2 - this.x1) * (this.x2 - this.x1),
+			(this.y2 - this.y1) * (this.y2 - this.y1),
+			(this.z2 - this.z1) * (this.z2 - this.z1)
+		);
+		let c = Math.sqrt(
+			(this.x3 - this.x2) * (this.x3 - this.x2),
+			(this.y3 - this.y2) * (this.y3 - this.y2),
+			(this.z3 - this.z2) * (this.z3 - this.z2)
+		);
 
-		let cosBC = ((-1) * a ** 2 + b ** 2 + c ** 2) / (2 * b * c);
-		let cosCA = (a ** 2 - b ** 2 + c ** 2) / (2 * c * a);
-		let cosAB = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b);
+		let cosBC = (-1 * a * a + b * b + c * c) / (2 * b * c);
+		let cosCA = (a * a - b * b + c * c) / (2 * c * a);
+		let cosAB = (a * a + b * b - c * c) / (2 * a * b);
 
 		let alpha = Math.acos(cosBC);
 		let beta = Math.acos(cosCA);
 		let gama = Math.acos(cosAB);
 
-		let P1 = {s: 0, t: length_t};
-		let P2 = {s: c - a * cosCA, t: length_t - a * Math.sin(beta)};
-		let P3 = {s: c, t: length_t};
+		let P1 = { s: 0, t: length_t };
+		let P2 = { s: c - a * cosCA, t: length_t - a * Math.sin(beta) };
+		let P3 = { s: c, t: length_t };
 
 		this.vertices = [
-			this.x1, this.y1, this.z1,
-			this.x2, this.y2, this.z2,
-			this.x3, this.y3, this.z3
+			this.x1,
+			this.y1,
+			this.z1,
+			this.x2,
+			this.y2,
+			this.z2,
+			this.x3,
+			this.y3,
+			this.z3
 		];
 
 		this.indices = [0, 1, 2];
 
-		this.normals = [
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1
-		];
+		this.normals = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1];
 
-		this.texCoords = [
-			P1.s, P1.t,
-			P2.s, P2.t,
-			P3.s, P3.t
-		];
+		this.texCoords = [P1.s, P1.t, P2.s, P2.t, P3.s, P3.t];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
