c1c1993056811fef16a6cd59efc3ebb114247a5a
BaltasarAroso <ba_aroso@icloud.com>
2018-10-15 11:09:02 +0100

    add triangle

--- T1/t1/MyCircle.js
+++ T1/t1/MyCircle.js
@@ -14,28 +14,35 @@ class MyCircle extends CGFobject {
 		this.normals = [];
 		this.texCoords = [];
 
-		this.angle = 2*Math.PI/this.slices;
+		this.angle = (2 * Math.PI) / this.slices;
 
 		this.initBuffers();
-	};
+	}
 
 	initBuffers() {
 		//center point
 		this.vertices.push(0, 0, 0);
 		this.normals.push(0, 0, 1);
-		this.texCoords.push(this.radius/2, this.radius/2);
-		for(let i = 0; i <= this.slices; i++) {
-			this.vertices.push(Math.cos(i*this.angle), Math.sin(i*this.angle), 0);
-			this.texCoords.push(this.radius/2 + this.radius/2*Math.cos(i*this.angle), this.radius/2 - this.radius/2*Math.sin(i*this.angle));
+		this.texCoords.push(this.radius / 2, this.radius / 2);
+		for (let i = 0; i <= this.slices; i++) {
+			this.vertices.push(
+				this.radius * Math.cos(i * this.angle),
+				this.radius * Math.sin(i * this.angle),
+				0
+			);
+			this.texCoords.push(
+				this.radius / 2 + (this.radius / 2) * Math.cos(i * this.angle),
+				this.radius / 2 - (this.radius / 2) * Math.sin(i * this.angle)
+			);
 			// this.normals.push(Math.cos(i*this.angle), Math.sin(i*this.angle), 0); //pointing outward
-			this.normals.push(0, 0, 1);  //pointing forward (in z)
+			this.normals.push(0, 0, 1); //pointing forward (in z)
 
-			if(i < this.slices) {
-				this.indices.push(0, i+1, i+2);
+			if (i < this.slices) {
+				this.indices.push(0, i + 1, i + 2);
 			}
 		}
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
-	};
+	}
 }
---
+++ T1/t1/MyTriangle.js
@@ -0,0 +1,40 @@
+/**
+ * MyTriangle
+ * @constructor
+ */
+class MyTriangle extends CGFobject {
+	constructor(scene, size = 1) {
+		super(scene);
+		this.size = size;
+		this.initBuffers();
+	};
+
+	initBuffers() {
+		this.vertices = [
+			- this.size/2, 0, 0,
+			this.size/2, 0, 0,
+			0, this.size * Math.sin(30 * DEGREE_TO_RAD), 0
+		];
+
+		this.indices = [
+			0, 1, 2
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+		];
+
+		this.texCoords = [
+			this.minS, this.maxT,
+			this.maxS, this.maxT,
+			this.minS, this.minT,
+			this.maxS, this.minT
+		];
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	};
+}
