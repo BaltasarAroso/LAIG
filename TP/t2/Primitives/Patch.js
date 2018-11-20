c2284b7ef77ac02fed47c927edb5c4db5e88dbed
BaltasarAroso <ba_aroso@icloud.com>
2018-11-20 12:35:31 +0000

    correct Plane orientation

--- TP/t2/Primitives/Patch.js
+++ TP/t2/Primitives/Patch.js
@@ -3,10 +3,10 @@
  * @constructor
  */
 class Patch extends CGFobject {
-	constructor(scene, npartsU, npartsV, npointsU, npointsV, controlpoints) {
+	constructor(scene, npartsU, npartsV, npointsU, npointsV, controlvertexes) {
 		super(scene);
 		
-		var nurbsSurface = new CGFnurbsSurface(npointsU, npointsV, controlpoints);
+		var nurbsSurface = new CGFnurbsSurface(npointsU, npointsV, controlvertexes);
 		this.obj = new CGFnurbsObject(this.scene, npartsU, npartsV, nurbsSurface); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
 	}
 	
